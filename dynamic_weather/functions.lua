bba94f7d87ee368171d5dae565c6e80ed27b513d
Patryk Mastalerczyk <patmast56@gmail.com>
2023-11-03 16:32:15 +0100

    Minor weather code changes, moved functions to separate file

---
+++ dynamic_weather/functions.lua
@@ -0,0 +1,28 @@
+local current_weather = nil
+
+local weather_list = {14, 15, 16}
+
+function change_weather(weather_id)
+    if (weather_id == nil) then
+        local random_index = math.random(1, #weather_list)
+        local random_weather = weather_list[random_index]
+
+        if not (random_weather == current_weather) then
+            local weather_change_interval = math.random(60, 120)
+
+            setWeather(random_weather)
+            current_weather = random_weather
+
+            local weather_change_timer = setTimer(function()
+                change_weather()
+            end, 1000 * 60 * weather_change_interval, 0)
+        end
+    else
+        setWeather(weather_id)
+        current_weather = weather_list[weather_id]
+    end
+end
+
+function get_current_weather()
+    return current_weather
+end
