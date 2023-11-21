869b46c6a859e1d8c7d738a97031a75c1dbe76cb
Patryk Mastalerczyk <patmast56@gmail.com>
2023-11-21 21:12:51 +0100

    Client data validation

--- interior_system/server/events.lua
+++ interior_system/server/events.lua
@@ -4,9 +4,11 @@ end)
 
 addEvent("on_player_request_interior_list", true)
 addEventHandler("on_player_request_interior_list", root, function(player)
-    local interior_list = get_interiors_data()
-    
-    if (interior_list) then
-        triggerClientEvent(player, "on_player_receive_interior_list", player, interior_list)
+    if (player and getElementType(player) == "player") then
+        local interior_list = get_interiors_data()
+        
+        if (interior_list) then
+            triggerClientEvent(player, "on_player_receive_interior_list", player, interior_list)
+        end
     end
 end)
